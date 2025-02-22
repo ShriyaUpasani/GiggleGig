dc963ce1132f6c3e7efede15d51156a07e2d578e
Shriya Upasani <shriyamauli@gmail.com>
2025-02-22 03:58:44 -0800

    deployment1

---
+++ components/hero.jsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const HeroSection = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10 overflow-hidden">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl xl:text-7xl font-serif">
+            GiggleGig
+          </h1>
+          <br />
+          <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl xl:text-6xl gradient-title animate-gradient">
+            Your AI Career Coach for
+            <br />
+            Professional Success
+          </h1>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
+          </p>
+        </div>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+          {/* <Link href="">
+            <Button size="lg" variant="outline" className="px-8">
+              Watch Demo
+            </Button>
+          </Link> */}
+        </div>
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/banner3.jpeg"
+              width={1280}
+              height={720}
+              alt="Dashboard Preview"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
